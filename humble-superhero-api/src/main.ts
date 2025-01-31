cafafba851720ed759cde8d9668ba2d8c4c30bf1
Emanuel Peña <emanuel.cuevas03@gmail.com>
2025-01-31 18:46:40 +0100

    add: SuperheroesAPI logger

--- humble-superhero-api/src/main.ts
+++ humble-superhero-api/src/main.ts
@@ -1,17 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const LOGGER = new Logger('SuperheroesAPI');
+
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
 
   await app.listen(process.env.PORT ?? 3000);
+  LOGGER.log(`Server running on port ${process.env.PORT || 3000}`);
 }
 bootstrap();
